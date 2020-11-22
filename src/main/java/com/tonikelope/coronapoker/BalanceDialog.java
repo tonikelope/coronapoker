56c24c027a25369a530ded78d913fc646cb89b91
tonikelope <tonikelope@gmail.com>
2020-11-22 16:07:22 +0100

    5.62
    Balance panel height fix

--- src/main/java/com/tonikelope/coronapoker/BalanceDialog.java
+++ src/main/java/com/tonikelope/coronapoker/BalanceDialog.java
@@ -115,7 +115,7 @@ public class BalanceDialog extends javax.swing.JDialog {
 
                 }
 
-                setPreferredSize(new Dimension(jugadores.getWidth(), Math.round(0.6f * getParent().getHeight())));
+                setPreferredSize(new Dimension(jugadores.getWidth(), Math.round(0.7f * getParent().getHeight())));
 
                 Helpers.updateFonts(tthis, Helpers.GUI_FONT, null);
 

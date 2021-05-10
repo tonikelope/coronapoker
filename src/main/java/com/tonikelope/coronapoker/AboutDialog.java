5c2725b6fae7b0c316dc5f49ca78421549a3469f
tonikelope <tonikelope@gmail.com>
2021-05-10 19:47:05 +0200

    8.93
    HUD fix

--- src/main/java/com/tonikelope/coronapoker/AboutDialog.java
+++ src/main/java/com/tonikelope/coronapoker/AboutDialog.java
@@ -16,7 +16,7 @@ import javax.swing.ImageIcon;
  */
 public class AboutDialog extends javax.swing.JDialog {
 
-    public static final String VERSION = "8.92";
+    public static final String VERSION = "8.93";
     public static final String UPDATE_URL = "https://github.com/tonikelope/coronapoker/releases/latest";
     public static final String TITLE = "¿De dónde ha salido esto?";
     public static final int MAX_MOD_LOGO_HEIGHT = 75;

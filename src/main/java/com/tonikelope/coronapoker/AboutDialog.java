b48c6fc19eb68acbd5d59f0d9992ddfcdbcb595e
tonikelope <tonikelope@gmail.com>
2022-02-01 15:17:15 +0100

    12.67
    TTS chat clean fix

--- src/main/java/com/tonikelope/coronapoker/AboutDialog.java
+++ src/main/java/com/tonikelope/coronapoker/AboutDialog.java
@@ -30,7 +30,7 @@ import javax.swing.SwingUtilities;
  */
 public class AboutDialog extends javax.swing.JDialog {
 
-    public static final String VERSION = "12.66";
+    public static final String VERSION = "12.67";
     public static final String UPDATE_URL = "https://github.com/tonikelope/coronapoker/releases/latest";
     public static final String TITLE = "¿De dónde ha salido esto?";
     public static final int MAX_MOD_LOGO_HEIGHT = 75;

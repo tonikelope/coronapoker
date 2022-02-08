64a51668084740077ce59bd87f9d7e7ccbae004f
tonikelope <tonikelope@gmail.com>
2022-02-08 23:23:42 +0100

    13.7
    tts chat recibidos nick fix
    10 a 5 hilos en carga de gifs.

--- src/main/java/com/tonikelope/coronapoker/AboutDialog.java
+++ src/main/java/com/tonikelope/coronapoker/AboutDialog.java
@@ -32,7 +32,7 @@ import javax.swing.SwingUtilities;
  */
 public class AboutDialog extends javax.swing.JDialog {
 
-    public static final String VERSION = "13.6";
+    public static final String VERSION = "13.7";
     public static final String UPDATE_URL = "https://github.com/tonikelope/coronapoker/releases/latest";
     public static final String TITLE = "¿De dónde ha salido esto?";
     public static final int MAX_MOD_LOGO_HEIGHT = 75;

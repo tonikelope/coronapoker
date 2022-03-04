c190e42599c24c5ca460c85fe9dbf856ff488ecf
tonikelope <tonikelope@gmail.com>
2022-03-04 19:22:54 +0100

    14.17
    Chat repaint.

--- src/main/java/com/tonikelope/coronapoker/AboutDialog.java
+++ src/main/java/com/tonikelope/coronapoker/AboutDialog.java
@@ -35,7 +35,7 @@ import javax.swing.Timer;
  */
 public class AboutDialog extends javax.swing.JDialog {
 
-    public static final String VERSION = "14.16";
+    public static final String VERSION = "14.17";
     public static final String UPDATE_URL = "https://github.com/tonikelope/coronapoker/releases/latest";
     public static final String TITLE = "¿De dónde ha salido esto?";
     public static final int MAX_MOD_LOGO_HEIGHT = 75;

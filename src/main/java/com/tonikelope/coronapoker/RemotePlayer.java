fda456b49f14043c34cf890b7bfc9fd939589dec
tonikelope <tonikelope@gmail.com>
2022-05-10 19:46:28 +0200

    15.7
    ALL IN destapar resistencia icon.

--- src/main/java/com/tonikelope/coronapoker/RemotePlayer.java
+++ src/main/java/com/tonikelope/coronapoker/RemotePlayer.java
@@ -2157,7 +2157,7 @@ public class RemotePlayer extends JPanel implements ZoomableInterface, Player {
                 player_action.setBackground(ganador ? new Color(120, 200, 0) : new Color(230, 70, 0));
                 player_action.setForeground(ganador ? Color.BLACK : Color.WHITE);
                 player_action.setText(jugada.getName());
-                setPlayerActionIcon(null);
+                setPlayerActionIcon("action/miedo.png");
 
             }
         });

ba4ca865c32a90b0320e46a85ddf05b5b41ec7bb
tonikelope <tonikelope@gmail.com>
2022-07-12 19:14:52 +0200

    16.9
    Colores botones fix.

--- src/main/java/com/tonikelope/coronapoker/Helpers.java
+++ src/main/java/com/tonikelope/coronapoker/Helpers.java
@@ -2400,7 +2400,7 @@ public class Helpers {
 
     public static void pausar(long pause) {
         try {
-            Thread.sleep(Math.max(pause,0));
+            Thread.sleep(Math.max(pause, 0));
         } catch (InterruptedException ex) {
             Logger.getLogger(Helpers.class.getName()).log(Level.SEVERE, null, ex);
         }

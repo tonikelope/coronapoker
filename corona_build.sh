cfdefa9541dd9f27aa7dc2d7f8ce6e538cbfa43a
tonikelope <tonikelope@gmail.com>
2022-10-22 23:51:45 +0200

    ...

--- corona_build.sh
+++ corona_build.sh
@@ -27,28 +27,28 @@ fi
 
 cd /home/tonikelope/TRASTERO/CoronaPokerReleases/
 
+if [ ! -f CoronaPoker_"${RELEASE_VERSION}".jar ]; then
+	cp -f /home/tonikelope/TRASTERO/CoronaPoker/target/CoronaPoker-"${RELEASE_VERSION}"-jar-with-dependencies.jar /home/tonikelope/TRASTERO/CoronaPokerReleases/CoronaPoker_"${RELEASE_VERSION}".jar
+fi
+
 if [ ! -f CoronaPokerLINUX_"${RELEASE_VERSION}"_portable.zip ]; then
-	cp -f /home/tonikelope/TRASTERO/CoronaPoker/target/CoronaPoker-"${RELEASE_VERSION}"-jar-with-dependencies.jar /home/tonikelope/TRASTERO/CoronaPokerReleases/CoronaPokerLINUX/jar/CoronaPoker.jar
+	cp -f /home/tonikelope/TRASTERO/CoronaPokerReleases/CoronaPoker_"${RELEASE_VERSION}".jar /home/tonikelope/TRASTERO/CoronaPokerReleases/CoronaPokerLINUX/jar/CoronaPoker.jar
 	zip -r CoronaPokerLINUX_"${RELEASE_VERSION}"_portable.zip CoronaPokerLINUX/
 fi
 
 if [ ! -f CoronaPokerMACOS_"${RELEASE_VERSION}"_portable.zip ]; then
-	cp -f /home/tonikelope/TRASTERO/CoronaPoker/target/CoronaPoker-"${RELEASE_VERSION}"-jar-with-dependencies.jar /home/tonikelope/TRASTERO/CoronaPokerReleases/CoronaPokerMACOS/CoronaPoker.app/Contents/MacOS/jar/CoronaPoker.jar
+	cp -f /home/tonikelope/TRASTERO/CoronaPokerReleases/CoronaPoker_"${RELEASE_VERSION}".jar /home/tonikelope/TRASTERO/CoronaPokerReleases/CoronaPokerMACOS/CoronaPoker.app/Contents/MacOS/jar/CoronaPoker.jar
 	zip -r CoronaPokerMACOS_"${RELEASE_VERSION}"_portable.zip CoronaPokerMACOS/
 fi
 
 if [ ! -f CoronaPokerWINDOWS32_"${RELEASE_VERSION}"_portable.zip ]; then
-	cp -f /home/tonikelope/TRASTERO/CoronaPoker/target/CoronaPoker-"${RELEASE_VERSION}"-jar-with-dependencies.jar /home/tonikelope/TRASTERO/CoronaPokerReleases/CoronaPokerWINDOWS32/jar/CoronaPoker.jar
+	cp -f /home/tonikelope/TRASTERO/CoronaPokerReleases/CoronaPoker_"${RELEASE_VERSION}".jar /home/tonikelope/TRASTERO/CoronaPokerReleases/CoronaPokerWINDOWS32/jar/CoronaPoker.jar
 	zip -r CoronaPokerWINDOWS32_"${RELEASE_VERSION}"_portable.zip CoronaPokerWINDOWS32/
 fi
 
 if [ ! -f CoronaPokerWINDOWS_"${RELEASE_VERSION}"_portable.zip ]; then
-	cp -f /home/tonikelope/TRASTERO/CoronaPoker/target/CoronaPoker-"${RELEASE_VERSION}"-jar-with-dependencies.jar /home/tonikelope/TRASTERO/CoronaPokerReleases/CoronaPokerWINDOWS/jar/CoronaPoker.jar
+	cp -f /home/tonikelope/TRASTERO/CoronaPokerReleases/CoronaPoker_"${RELEASE_VERSION}".jar /home/tonikelope/TRASTERO/CoronaPokerReleases/CoronaPokerWINDOWS/jar/CoronaPoker.jar
 	zip -r CoronaPokerWINDOWS_"${RELEASE_VERSION}"_portable.zip CoronaPokerWINDOWS/
-fi
-
-if [ ! -f CoronaPoker_"${RELEASE_VERSION}".jar ]; then
-	cp -f /home/tonikelope/TRASTERO/CoronaPoker/target/CoronaPoker-"${RELEASE_VERSION}"-jar-with-dependencies.jar /home/tonikelope/TRASTERO/CoronaPokerReleases/CoronaPoker_"${RELEASE_VERSION}".jar
 fi
 
 echo -e "\nCREANDO RELEASE v${RELEASE_VERSION} en github..."

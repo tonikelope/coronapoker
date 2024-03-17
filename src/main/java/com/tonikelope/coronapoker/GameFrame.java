f7c1f5b26c4da421ea46dff41628ad3a6767012f
tonikelope <tonikelope@gmail.com>
2024-03-17 16:38:30 +0100

    17.59
    RADAR default off

--- src/main/java/com/tonikelope/coronapoker/GameFrame.java
+++ src/main/java/com/tonikelope/coronapoker/GameFrame.java
@@ -164,7 +164,7 @@ public final class GameFrame extends javax.swing.JFrame implements ZoomableInter
     public static volatile boolean AUTO_ZOOM = Boolean.parseBoolean(Helpers.PROPERTIES.getProperty("auto_zoom", "false"));
     public static volatile boolean LOCAL_POSITION_CHIP = Boolean.parseBoolean(Helpers.PROPERTIES.getProperty("local_pos_chip", "true"));
     public static volatile String SERVER_HISTORY = Helpers.PROPERTIES.getProperty("server_history", "");
-    public static volatile boolean RADAR_AVAILABLE = Boolean.parseBoolean(Helpers.PROPERTIES.getProperty("radar", "true"));
+    public static volatile boolean RADAR_AVAILABLE = Boolean.parseBoolean(Helpers.PROPERTIES.getProperty("radar", "false"));
     public static volatile boolean RECOVER = false;
     public static volatile Boolean MAC_NATIVE_FULLSCREEN = null;
     public static volatile boolean TTS_SERVER = true;

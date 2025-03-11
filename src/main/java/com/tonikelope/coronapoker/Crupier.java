3cdd1a54d944440085d51011f8681b4533605b82
tonikelope <tonikelope@gmail.com>
2025-03-11 21:17:00 +0100

    18.14
    ...

--- src/main/java/com/tonikelope/coronapoker/Crupier.java
+++ src/main/java/com/tonikelope/coronapoker/Crupier.java
@@ -28,6 +28,7 @@ https://github.com/tonikelope/coronapoker
  */
 package com.tonikelope.coronapoker;
 
+import com.drew.imaging.ImageProcessingException;
 import static com.tonikelope.coronapoker.Card.BARAJAS;
 import static com.tonikelope.coronapoker.GameFrame.WAIT_QUEUES;
 import java.awt.Color;
@@ -40,6 +41,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -2216,22 +2218,23 @@ public class Crupier implements Runnable {
 
                     if (GameFrame.CINEMATICAS) {
 
-                        Helpers.GUIRun(() -> {
-                            try {
-                                GifAnimationDialog gif_dialog = new GifAnimationDialog(GameFrame.getInstance().getFrame(), true, new ImageIcon(getClass().getResource("/cinematics/misc/iwtsth.gif")), Helpers.getGIFFramesCount(getClass().getResource("/cinematics/misc/iwtsth.gif").toURI().toURL()));
+                        try {
+                            GifAnimationDialog gif_dialog = new GifAnimationDialog(GameFrame.getInstance().getFrame(), true, new ImageIcon(getClass().getResource("/cinematics/misc/iwtsth.gif")), Helpers.getGIFFramesCount(getClass().getResource("/cinematics/misc/iwtsth.gif").toURI().toURL()));
+
+                            Helpers.GUIRun(() -> {
 
                                 gif_dialog.setLocationRelativeTo(gif_dialog.getParent());
 
                                 gif_dialog.setVisible(true);
 
-                            } catch (Exception ex) {
-                                Logger.getLogger(Crupier.class.getName()).log(Level.SEVERE, null, ex);
-                            }
-                        });
+                            });
 
-                        Helpers.pausar(500);
+                            Helpers.pausar(500);
 
-                        Audio.playWavResourceAndWait("misc/iwtsth.wav");
+                            Audio.playWavResourceAndWait("misc/iwtsth.wav");
+                        } catch (URISyntaxException | IOException | ImageProcessingException ex) {
+                            Logger.getLogger(Crupier.class.getName()).log(Level.SEVERE, null, ex);
+                        }
 
                     } else {
                         Audio.playWavResourceAndWait("misc/iwtsth.wav");
@@ -2358,16 +2361,18 @@ public class Crupier implements Runnable {
                             GameFrame.getInstance().getRegistro().print(Translator.translate("EL SERVIDOR HA DENEGADO LA SOLICITUD IWTSTH DE ") + iwtsther);
 
                             if (GameFrame.CINEMATICAS) {
+                                try {
+                                    GifAnimationDialog gif_dialog = new GifAnimationDialog(GameFrame.getInstance().getFrame(), true, new ImageIcon(getClass().getResource("/cinematics/misc/iwtsth_no.gif")), Helpers.getGIFFramesCount(getClass().getResource("/cinematics/misc/iwtsth_no.gif").toURI().toURL()));
 
-                                Helpers.GUIRun(() -> {
-                                    try {
-                                        GifAnimationDialog gif_dialog = new GifAnimationDialog(GameFrame.getInstance().getFrame(), true, new ImageIcon(getClass().getResource("/cinematics/misc/iwtsth_no.gif")), Helpers.getGIFFramesCount(getClass().getResource("/cinematics/misc/iwtsth_no.gif").toURI().toURL()));
+                                    Helpers.GUIRun(() -> {
+
                                         gif_dialog.setLocationRelativeTo(gif_dialog.getParent());
                                         gif_dialog.setVisible(true);
-                                    } catch (Exception ex) {
-                                        Logger.getLogger(Crupier.class.getName()).log(Level.SEVERE, null, ex);
-                                    }
-                                });
+
+                                    });
+                                } catch (URISyntaxException | IOException | ImageProcessingException ex) {
+                                    Logger.getLogger(Crupier.class.getName()).log(Level.SEVERE, null, ex);
+                                }
 
                             }
 
--- src/main/java/com/tonikelope/coronapoker/GameFrame.java
+++ src/main/java/com/tonikelope/coronapoker/GameFrame.java
@@ -28,6 +28,7 @@ https://github.com/tonikelope/coronapoker
  */
 package com.tonikelope.coronapoker;
 
+import com.drew.imaging.ImageProcessingException;
 import static com.tonikelope.coronapoker.Card.BARAJAS;
 import static com.tonikelope.coronapoker.Crupier.STREETS;
 import static com.tonikelope.coronapoker.Helpers.TapetePopupMenu.BARAJAS_MENU;
@@ -1531,24 +1532,29 @@ public final class GameFrame extends javax.swing.JFrame implements ZoomableInter
                     cambiarBaraja();
                     if (Init.M2 != null && GameFrame.BARAJA.equals("interstate60") && i60_c == 5) {
                         try {
-                            Files.write(Paths.get(System.getProperty("java.io.tmpdir") + "/M2e.gif"), (byte[]) M2.invoke(null, "e"));
-                        } catch (Exception ex) {
-                            Logger.getLogger(Init.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                        i60_c = 0;
-                        Helpers.GUIRunAndWait(() -> {
                             try {
-                                gif_dialog = new GifAnimationDialog(getFrame(), true, new ImageIcon(Files.readAllBytes(Paths.get(System.getProperty("java.io.tmpdir") + "/M2e.gif"))), Helpers.getGIFFramesCount(Paths.get(System.getProperty("java.io.tmpdir") + "/M2e.gif").toUri().toURL()));
+                                Files.write(Paths.get(System.getProperty("java.io.tmpdir") + "/M2e.gif"), (byte[]) M2.invoke(null, "e"));
+                            } catch (Exception ex) {
+                                Logger.getLogger(Init.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                            i60_c = 0;
+                            gif_dialog = new GifAnimationDialog(getFrame(), true, new ImageIcon(Files.readAllBytes(Paths.get(System.getProperty("java.io.tmpdir") + "/M2e.gif"))), Helpers.getGIFFramesCount(Paths.get(System.getProperty("java.io.tmpdir") + "/M2e.gif").toUri().toURL()));
+
+                            Helpers.GUIRunAndWait(() -> {
+
                                 gif_dialog.setLocationRelativeTo(gif_dialog.getParent());
                                 gif_dialog.setVisible(true);
-                            } catch (Exception ex) {
-                                Logger.getLogger(GameFrame.class.getName()).log(Level.SEVERE, null, ex);
+
+                            });
+                            try {
+                                Files.deleteIfExists(Paths.get(System.getProperty("java.io.tmpdir") + "/M2e.gif"));
+                            } catch (IOException ex) {
+                                Logger.getLogger(Init.class.getName()).log(Level.SEVERE, null, ex);
                             }
-                        });
-                        try {
-                            Files.deleteIfExists(Paths.get(System.getProperty("java.io.tmpdir") + "/M2e.gif"));
                         } catch (IOException ex) {
-                            Logger.getLogger(Init.class.getName()).log(Level.SEVERE, null, ex);
+                            Logger.getLogger(GameFrame.class.getName()).log(Level.SEVERE, null, ex);
+                        } catch (ImageProcessingException ex) {
+                            Logger.getLogger(GameFrame.class.getName()).log(Level.SEVERE, null, ex);
                         }
                     }
                 });
@@ -2054,12 +2060,15 @@ public final class GameFrame extends javax.swing.JFrame implements ZoomableInter
 
                 final URL f_url_icon = url_icon;
 
+                try {
+                    gif_dialog = new GifAnimationDialog(getFrame(), true, icon, Helpers.getGIFFramesCount(f_url_icon));
+                } catch (IOException ex) {
+                    Logger.getLogger(GameFrame.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (ImageProcessingException ex) {
+                    Logger.getLogger(GameFrame.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
                 Helpers.GUIRun(() -> {
-                    try {
-                        gif_dialog = new GifAnimationDialog(getFrame(), true, icon, Helpers.getGIFFramesCount(f_url_icon));
-                    } catch (Exception ex) {
-                        Logger.getLogger(GameFrame.class.getName()).log(Level.SEVERE, null, ex);
-                    }
 
                     gif_dialog.addMouseListener(new MouseAdapter() {
                         @Override

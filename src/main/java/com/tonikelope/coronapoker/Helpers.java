9d6d566d6949dae5364dd889b8d26a9bf891f41f
tonikelope <tonikelope@gmail.com>
2026-04-28 01:16:15 +0200

    20.29

--- src/main/java/com/tonikelope/coronapoker/Helpers.java
+++ src/main/java/com/tonikelope/coronapoker/Helpers.java
@@ -3749,7 +3749,7 @@ public class Helpers {
             selectAll.setIcon(new javax.swing.ImageIcon(Helpers.class.getResource("/images/menu/select_all.png")));
             popup.add(selectAll);
 
-             Helpers.updateFonts(popup, Helpers.GUI_FONT, Float.valueOf(Helpers.PROPERTIES.getProperty("zoom_menu", "1")));
+            Helpers.updateFonts(popup, Helpers.GUI_FONT, Float.valueOf(Helpers.PROPERTIES.getProperty("zoom_menu", "1")));
             txtField.setComponentPopupMenu(popup);
         }
 
@@ -3817,7 +3817,7 @@ public class Helpers {
             JMenuItem selectAll = new LeftClickMenuItem(selectAllAction);
             selectAll.setIcon(new javax.swing.ImageIcon(Helpers.class.getResource("/images/menu/select_all.png")));
             popup.add(selectAll);
-             Helpers.updateFonts(popup, Helpers.GUI_FONT, Float.valueOf(Helpers.PROPERTIES.getProperty("zoom_menu", "1")));
+            Helpers.updateFonts(popup, Helpers.GUI_FONT, Float.valueOf(Helpers.PROPERTIES.getProperty("zoom_menu", "1")));
             txtArea.setComponentPopupMenu(popup);
         }
 
@@ -3885,7 +3885,7 @@ public class Helpers {
             JMenuItem selectAll = new LeftClickMenuItem(selectAllAction);
             selectAll.setIcon(new javax.swing.ImageIcon(Helpers.class.getResource("/images/menu/select_all.png")));
             popup.add(selectAll);
-             Helpers.updateFonts(popup, Helpers.GUI_FONT, Float.valueOf(Helpers.PROPERTIES.getProperty("zoom_menu", "1")));
+            Helpers.updateFonts(popup, Helpers.GUI_FONT, Float.valueOf(Helpers.PROPERTIES.getProperty("zoom_menu", "1")));
             txtArea.setComponentPopupMenu(popup);
         }
 

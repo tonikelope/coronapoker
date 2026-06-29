d2431cf2b2598247611e3e678f43b4bb3ae4c275
tonikelope <tonikelope@gmail.com>
2026-06-29 21:18:28 +0200

    chore(release): CoronaPoker 22.7

--- src/main/java/com/tonikelope/coronapoker/AboutDialog.java
+++ src/main/java/com/tonikelope/coronapoker/AboutDialog.java
@@ -71,7 +71,7 @@ import javax.swing.Timer;
  */
 public class AboutDialog extends JDialog {
 
-    public static final String VERSION = "22.6";
+    public static final String VERSION = "22.7";
     public static final String UPDATE_URL = "https://github.com/tonikelope/coronapoker/releases/latest";
 
     public static final String TITLE = "about.titulo";

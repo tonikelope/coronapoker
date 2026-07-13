881c8d965d73db3d4b6df5a25adecd803ea1e8df
tonikelope <tonikelope@gmail.com>
2026-07-13 19:56:14 +0200

    chore(release): CoronaPoker 22.68 (zoom global de dialogos)
    Zoom de dialogos configurable (Ajustes > Apariencia, solo desde el inicio, transaccional):
    escala fuentes, iconos, botones, huecos y el propio tamano de la ventana en todos los
    dialogos del juego (Salir, Nueva partida, Ajustes, sala de espera, registro, estadisticas,
    rebuy, straddle, RIT, reconnect, blinds, pausa, recover, volumen, identicons, imagenes de
    chat...). Iconos de menu/popup y JOptionPane incluidos; el zoom por defecto de las graficas
    de Stats sigue el de dialogos. Canario de test que detecta si una regeneracion de .form en
    NetBeans se lleva el escalado.

--- src/main/java/com/tonikelope/coronapoker/AboutDialog.java
+++ src/main/java/com/tonikelope/coronapoker/AboutDialog.java
@@ -69,7 +69,7 @@ import javax.swing.Timer;
  */
 public class AboutDialog extends JDialog {
 
-    public static final String VERSION = "22.67";
+    public static final String VERSION = "22.68";
     public static final String UPDATE_URL = "https://github.com/tonikelope/coronapoker/releases/latest";
 
     public static final String TITLE = "about.titulo";
